b53579899a93c980955d11fa5cc89f0fc04ddee8
ondrej.zara <none@none>
2010-05-19 20:00:08 +0000

    zip listing
    --HG--
    extra : convert_revision : svn%3Adf6326ff-81f5-47bb-bc06-55820a61e8ec/trunk%40112

--- firecommander@ondras.zarovi.cz/chrome/content/path/path-zip.js
+++ firecommander@ondras.zarovi.cz/chrome/content/path/path-zip.js
@@ -1,5 +1,5 @@
 /**
- * @param {nsIFile} file
+ * @param {path} file
  * @param {string} name Full path within archive
  * @param {nsIZipEntry || null} entry
  * @param {FC} fc
@@ -10,6 +10,12 @@ Path.Zip = function(file, name, entry, fc) {
 	this._entry = entry;
 	this._fc = fc;
 	this._zip = Cc["@mozilla.org/libjar/zip-reader;1"].createInstance(Ci.nsIZipReader);
+	
+	if (!this._entry && this._file.exists() && this._name) {
+		this._zip.open(this._file.getFile());
+		this._entry = this._zip.getEntry(this._name);
+		this._zip.close();
+	}
 }
 
 Path.Zip.prototype = Object.create(Path.prototype);
@@ -28,7 +34,6 @@ Path.Zip.fromString = function(path, fc) {
 	
 	name = name.join("/");
 	if (path.charAt(path.length-1) == "/" && name.charAt(name.length-1) != "/") { name += "/"; }
-	
 	return new this(local, name, null, fc);
 }
 
@@ -45,11 +50,23 @@ Path.Zip.prototype.getName = function() {
 }
 
 Path.Zip.prototype.getImage = function() {
-	/* FIXME */
+	if (this._entry.isDirectory) {
+		return "chrome://firecommander/skin/folder.png";
+	} else {
+		var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+		var pseudoName = this._file.getPath() + "/" + this._name;
+		var pseudoPath = Path.Local.fromString(pseudoName);
+		var fileURI = ios.newFileURI(pseudoPath.getFile());
+		return "moz-icon://" + fileURI.spec;
+	}
 }
 
 Path.Zip.prototype.getSize = function() {
 	return this._entry.realSize;
+}
+
+Path.Zip.prototype.getSort = function() {
+	return (this._entry.isDirectory ? 1 : 2);
 }
 
 Path.Zip.prototype.getTS = function() {
@@ -57,22 +74,19 @@ Path.Zip.prototype.getTS = function() {
 }
 
 Path.Zip.prototype.getParent = function() {
-	if (!this._name) { return new Path.Local(this._file); }
+	if (!this._name) { return this._file.getParent(); }
 	var parts = this._name.split("/");
 	parts.pop() || parts.pop(); /* remove last non-empty part */
-	var parentName = parts.join("/") + "/";
-	
+	var parentName = (parts.length ? parts.join("/") + "/" : "");
 	return new Path.Zip(this._file, parentName, null, this._fc);
 }
 
 Path.Zip.prototype.getItems = function() {
 	var results = [];
-	
-	this._zip.open(this._file);
-	var entries = z.findEntries(null);
-	var filtered = this._getChildEntries(entries);
-	
-	var re = new RegExp("^" + this._name + "[^/]+" + "/?");
+		
+	this._zip.open(this._file.getFile());
+	var entries = this._zip.findEntries(null);	
+	var re = new RegExp("^" + this._name + "[^/]+" + "/?$");
 	while (entries.hasMore()) {
 		var entry = entries.getNext();
 		if (!entry.match(re)) { continue; }
@@ -86,8 +100,9 @@ Path.Zip.prototype.getItems = function() {
 
 Path.Zip.prototype.exists = function() {
 	if (!this._file.exists()) { return false; }
+	if (!this._name) { return true; } /* root entry */
 	
-	this._zip.open(this._file);
+	this._zip.open(this._file.getFile());
 	var result = this._zip.hasEntry(this._name);
 	this._zip.close();
 
@@ -96,13 +111,16 @@ Path.Zip.prototype.exists = function() {
 
 Path.Zip.prototype.supports = function(feature) {
 	/* FIXME */
-	if (feature == FC.CHILDREN) { return this._entry.isDirectory(); }
+	if (feature == FC.CHILDREN) { 
+		if (!this._name) { return true; }
+		return this._entry.isDirectory; 
+	}
 
 	return false;
 }
 
 Path.Zip.prototype.activate = function(panel) {
-	if (this._entry.isDirectory()) { panel.setPath(this); }
+	if (!this._name || this._entry.isDirectory) { panel.setPath(this); }
 }
 
 FC.addProtocolHandler("zip", Path.Zip.fromString.bind(Path.Zip));
