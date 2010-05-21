9785744fa85514088fcb8649fe65d6bbf5645c9f
ondrej.zara <none@none>
2010-05-21 13:12:46 +0000

    xulrunner experiments
    --HG--
    extra : convert_revision : svn%3Adf6326ff-81f5-47bb-bc06-55820a61e8ec/trunk%40122

--- makexpi.sh
+++ makexpi.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
+APPDIR=firecommander@ondras.zarovi.cz
+VERSION=`cat $APPDIR/install.rdf | grep em:version | sed "s/[^0-9.]//g"`
 
-cd firecommander@ondras.zarovi.cz
-VERSION=`cat install.rdf | grep em:version | sed "s/[^0-9.]//g"`
-zip -r ../firecommander-${VERSION}.xpi * -x *.svn*
+### xpi
+mkdir xpi
+cp -r $APPDIR/* xpi
+find xpi -name .svn -print0 | xargs -0 rm -rf
+cd xpi
+rm application.ini
+zip -r ../firecommander-${VERSION}.xpi *
 cd ..
+rm -r xpi
+
+### xulrunner variant
+mkdir xr
+cp -r $APPDIR/* xr
+find xr -name .svn -print0 | xargs -0 rm -rf
+cd xr
+rm install.rdf
+
+# application.ini
+TS=`date +%s`
+sed -e "s,__VERSION__,$VERSION," \
+	-e "s,__TS__,$TS," \
+application.ini > application.ini.new
+mv application.ini.new application.ini
+
+# chrome manifest
+mv chrome.manifest chrome/
+
+# preferences
+
+zip -r ../firecommander-${VERSION}-xr.zip *
+cd ..
+rm -r xr
