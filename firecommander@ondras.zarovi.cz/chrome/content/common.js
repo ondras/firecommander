f6c7d3e3d9ea6dba2465e6fe619a843a0c76be5a
ondrej.zara <none@none>
2010-05-23 18:58:36 +0000

    refactored object.create and function.prototype.bind
    --HG--
    extra : convert_revision : svn%3Adf6326ff-81f5-47bb-bc06-55820a61e8ec/trunk%40138

--- firecommander@ondras.zarovi.cz/chrome/content/common.js
+++ firecommander@ondras.zarovi.cz/chrome/content/common.js
@@ -2,12 +2,17 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cr = Components.results;
 
-Function.prototype.bind = function(context) {
-	var t = this;
-	return function() { return t.apply(context, arguments); }
+Function.prototype.bind = Function.prototype.bind || function(context) {
+    var t = this, a = arguments;
+    return function() {
+        var args = [], i;
+        for (i=1; i<a.length; i++) { args.push(a[i]); }
+        for (i=0; i<arguments.length; i++) { args.push(arguments[i]); }
+        return t.apply(context, args);
+    }
 }
 
-Object.create = function(proto) {
+Object.create = Object.create || function(proto) {
 	var f = function() {};
 	f.prototype = proto;
 	return new f();
